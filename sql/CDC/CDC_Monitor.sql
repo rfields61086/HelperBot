a69fc8c9a5ab0f98e28e006484bd8814c945ffdd
rfields1986 <r.fie.lds61086@gmail.com>
2024-12-10 14:28:34 -0600

    new file

--- sql/CDC/CDC_Monitor.sql
+++ sql/CDC/CDC_Monitor.sql
@@ -21,76 +21,94 @@ ORDER BY h.run_date DESC, h.run_time DESC;
 GO
 
 -- Retrieve CDC log scan session errors
-SELECT 
-    capture_instance AS CaptureInstance,
-    log_scan_start_time AS LogScanStartTime,
-    log_scan_end_time AS LogScanEndTime,
-    log_scan_error AS LogScanErrorCode,
-    CASE 
-        WHEN log_scan_error = 0 THEN 'No Error'
+SELECT
+    session_id AS SessionID,
+    start_time AS StartTime,
+    end_time AS EndTime,
+    error_count AS ErrorCount,
+    CASE
+        WHEN error_count = 0 THEN 'No Error'
         ELSE 'Error Detected'
     END AS ErrorDescription,
-    log_scan_error_message AS ErrorMessage
+    latency AS LatencyInSeconds,
+    failed_sessions_count AS FailedSessionsCount
 FROM sys.dm_cdc_log_scan_sessions
-WHERE log_scan_error <> 0 -- Only show sessions with errors
-ORDER BY log_scan_start_time DESC;
+WHERE error_count > 0 -- Only show sessions with errors
+ORDER BY start_time DESC;
 GO
 
+
 -- Retrieve CDC-specific errors from SQL Server Error Log
-EXEC xp_readerrorlog 0, 1, 'cdc'; -- Searches the current error log for 'cdc'
+EXEC xp_readerrorlog 0, 1, N'cdc', NULL, NULL, NULL, N'desc';
 GO
 
 
 -- Retrieve CDC-enabled tables and their metadata
-SELECT 
-    cdc.name AS CaptureInstance,
-    s.name AS SchemaName,
-    t.name AS TableName,
-    cdc.create_date AS CDCEnableDate,
-    cdc.supports_net_changes AS SupportsNetChanges,
-    cdc.filegroup_name AS FileGroupName,
-    cdc.capture_instance AS CDCInstance,
-    cdc.source_object_id AS SourceObjectID,
-    cdc.start_lsn AS StartLSN
-FROM sys.tables AS t
-INNER JOIN sys.schemas AS s
-    ON t.schema_id = s.schema_id
-INNER JOIN sys.sp_cdc_enabled_tables() AS cdc
-    ON cdc.source_object_id = t.object_id
-ORDER BY SchemaName, TableName;
-GO
+EXEC sys.sp_cdc_help_change_data_capture;
+
 
 -- Retrieve information about CDC jobs
 SELECT 
-    job.name AS JobName,
-    cdc.state_desc AS State,
-    cdc.start_date AS JobStartDate,
-    cdc.last_run_date AS LastRunDate,
-    cdc.retention AS RetentionPeriodMinutes,
-    cdc.latency AS LatencyInSeconds
-FROM msdb.dbo.cdc_jobs AS cdc
-INNER JOIN msdb.dbo.sysjobs AS job
-    ON cdc.job_id = job.job_id;
+    sj.name AS JobName,
+    cj.job_type AS JobType,
+    cj.database_id AS DatabaseID,
+    DB_NAME(cj.database_id) AS DatabaseName,
+    sj.enabled AS IsEnabled,
+    sj.date_created AS DateCreated,
+    sj.date_modified AS DateModified,
+    cj.maxtrans AS MaxTransactions,
+    cj.maxscans AS MaxScans,
+    cj.continuous AS IsContinuous,
+    cj.pollinginterval AS PollingIntervalSeconds,
+    cj.retention AS RetentionPeriodMinutes,
+    cj.threshold AS Threshold
+FROM msdb.dbo.cdc_jobs AS cj
+INNER JOIN msdb.dbo.sysjobs AS sj
+    ON cj.job_id = sj.job_id
+ORDER BY cj.database_id, cj.job_type;
 GO
 
--- Monitor LSN ranges for CDC-enabled tables
-SELECT 
-    cdc.name AS CaptureInstance,
-    lsn.start_lsn AS StartLSN,
-    lsn.end_lsn AS EndLSN,
-    lsn.capture_time AS CaptureTime
-FROM sys.sp_cdc_enabled_tables() AS cdc
-CROSS APPLY sys.fn_cdc_get_min_lsn(cdc.name) AS lsn
-ORDER BY cdc.name, lsn.start_lsn;
+
+-- Retrieve LSN ranges and commit times for CDC-enabled tables
+SELECT
+    ct.capture_instance AS CaptureInstance,
+    ct.source_schema AS SchemaName,
+    ct.source_name AS TableName,
+    fn.cdc.fn_cdc_get_min_lsn(ct.capture_instance) AS MinLSN,
+    fn.cdc.fn_cdc_get_max_lsn() AS MaxLSN,
+    ltm.tran_begin_time AS MinLSNCommitTime,
+    ltm.tran_end_time AS MaxLSNCommitTime
+FROM
+    cdc.change_tables AS ct
+    CROSS APPLY (
+        SELECT
+            sys.fn_cdc_map_lsn_to_time(fn.cdc.fn_cdc_get_min_lsn(ct.capture_instance)) AS tran_begin_time,
+            sys.fn_cdc_map_lsn_to_time(fn.cdc.fn_cdc_get_max_lsn()) AS tran_end_time
+    ) AS ltm
+ORDER BY
+    ct.source_schema,
+    ct.source_name;
 GO
+
+
 
 -- Monitor CDC latency and log usage
-SELECT 
-    capture_instance AS CaptureInstance,
-    capture_time AS LastCaptureTime,
-    latency AS CurrentLatencyInSeconds,
-    filegroup_name AS CaptureFileGroup
+SELECT
+    session_id AS SessionID,
+    start_time AS StartTime,
+    end_time AS EndTime,
+    duration AS DurationSeconds,
+    scan_phase AS ScanPhase,
+    error_count AS ErrorCount,
+    tran_count AS TransactionCount,
+    last_commit_time AS LastCommitTime,
+    latency AS LatencySeconds,
+    log_record_count AS LogRecordCount,
+    schema_change_count AS SchemaChangeCount,
+    command_count AS CommandCount,
+    empty_scan_count AS EmptyScanCount,
+    failed_sessions_count AS FailedSessionsCount
 FROM sys.dm_cdc_log_scan_sessions
-WHERE capture_instance IS NOT NULL
-ORDER BY capture_time DESC;
+ORDER BY start_time DESC;
 GO
+
